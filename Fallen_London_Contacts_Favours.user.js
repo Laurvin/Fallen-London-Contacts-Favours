c01c97e462de778eba62446e7615fea57500747e
Laurvin <github@dbbv.com>
2018-11-24 08:32:36 +0100

    Update Fallen_London_Contacts_Favours.user.js

--- Fallen_London_Contacts_Favours.user.js
+++ Fallen_London_Contacts_Favours.user.js
@@ -3,7 +3,7 @@
 // @namespace Fallen London - Contacts Favours
 // @author Laurvin
 // @description Shows the Favours at the top of the page; you will need to refresh manually by clicking the bell icon.
-// @version 3.1
+// @version 3.2
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Fallen-London-Contacts-Favours/raw/master/Fallen_London_Contacts_Favours.user.js
 // @include https://fallenlondon.com/*
@@ -30,18 +30,18 @@ Favours['Favours: Revolutionaries'] = 0;
 Favours['Favours: Society'] = 0;
 
 var FactionIcon = {};
-FactionIcon['Favours: Criminals'] = 'manaclessmall';
-FactionIcon['Favours: The Docks'] = 'shipsmall';
-FactionIcon['Favours: Tomb-Colonies'] = 'tombcolonistsmall';
-FactionIcon['Favours: Rubbery Men'] = 'rubberymansmall';
-FactionIcon['Favours: Urchins'] = 'urchinsmall';
-FactionIcon['Favours: Hell'] = 'devilsmall';
-FactionIcon['Favours: Constables'] = 'constablebadgesmall';
-FactionIcon['Favours: The Great Game'] = 'pawnsmall';
-FactionIcon['Favours: The Church'] = 'clergysmall';
-FactionIcon['Favours: Bohemians'] = 'bohogirl1small';
-FactionIcon['Favours: Revolutionaries'] = 'flamessmall';
-FactionIcon['Favours: Society'] = 'salon2small';
+FactionIcon['Favours: Criminals'] = 'manacles';
+FactionIcon['Favours: The Docks'] = 'ship';
+FactionIcon['Favours: Tomb-Colonies'] = 'bandagedman';
+FactionIcon['Favours: Rubbery Men'] = 'rubberyman';
+FactionIcon['Favours: Urchins'] = 'urchin';
+FactionIcon['Favours: Hell'] = 'devil';
+FactionIcon['Favours: Constables'] = 'constablebadge';
+FactionIcon['Favours: The Great Game'] = 'pawn';
+FactionIcon['Favours: The Church'] = 'clergy';
+FactionIcon['Favours: Bohemians'] = 'bohogirl1';
+FactionIcon['Favours: Revolutionaries'] = 'flames';
+FactionIcon['Favours: Society'] = 'salon2';
 
 function addGlobalStyle(css)
 {
@@ -54,6 +54,7 @@ function addGlobalStyle(css)
     style.type = 'text/css';
     style.innerHTML = css;
     head.appendChild(style);
+    console.log("Hey!");
 }
 
 function addHTMLElements() // Adds a div for Contact icons and reload button, removes Fallen London logo.
@@ -92,10 +93,10 @@ function GetFavors()
             var CreatedHTML = "";
 
             $.each(Favours, function(faction, amount) {
-                CreatedHTML += '<div class="FLCFdivs"><img height="26" width="20" border="0" src="https://images.fallenlondon.com/icons/' + FactionIcon[faction] + '.png" /> ' + amount + '</div>';
+                CreatedHTML += '<div class="FLCFdivs"><img height="20" width="20" border="0" src="https://images.fallenlondon.com/images/icons_small/' + FactionIcon[faction] + '.png" /> ' + amount + '</div>';
             });
 
-            CreatedHTML += '<div class="FLCFdivs">&nbsp;</div><div class="FLCFdivs" id="FLCFreload" style="cursor:pointer"><img height="26" width="20" border="0" title="Reload" src="https://images.fallenlondon.com/icons/bellsmall.png" /></div>';
+            CreatedHTML += '<div class="FLCFdivs">&nbsp;</div><div class="FLCFdivs" id="FLCFreload" style="cursor:pointer"><img height="20" width="20" border="0" title="Reload" src="https://images.fallenlondon.com/images/icons_small/bell.png" /></div>';
 
             $("#FLCF").html(CreatedHTML);
 
@@ -114,9 +115,11 @@ function GetFavors()
 $(document).ready(function() {
     'use strict';
 
-    addGlobalStyle('#FLCF { width: 600px; margin-top: 7px; font-size: 14px; }');
-    addGlobalStyle('.FLCFdivs { float: left; width: 7%; }');
+//     addGlobalStyle('#FLCF { width: 600px; margin-top: 7px; font-size: 14px; }');
+//     addGlobalStyle('.FLCFdivs { float: left; width: 7%; }');
 
+    setTimeout(addGlobalStyle, 2 * 1000, '#FLCF { width: 600px; margin-top: 7px; font-size: 14px; }')
+    setTimeout(addGlobalStyle, 2 * 1000, '.FLCFdivs { float: left; width: 7%; }')
     setTimeout(addHTMLElements, 2 * 1000); // 2 seconds
-    setTimeout(GetFavors, 2 * 1000); // 2 seconds
+    setTimeout(GetFavors, 2 * 1000); // 2 seconds  
 });
